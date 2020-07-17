1b44452183858df17793dd5ec5df1f75a9c6acb4
Astgh78 <astghik.karakhanyan@gmail.com>
2020-07-17 17:12:59 +0400

    - added one more file to be ignored - added Coffee-Machine iml

--- Coffee-Machine/src/CoffeMachine.java
+++ Coffee-Machine/src/CoffeMachine.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class CoffeMachine {
+    public static void main(String[] args) {
+        int water = 200;
+        int milk = 50;
+        int coffee = 15;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please, enter the amount of needed coffee");
+        int cup = in.nextInt();
+
+        System.out.println("For " + cup + " cup of coffee we need " + cup * coffee + "g of coffee beans, " + cup * milk + "ml of milk, " + cup * coffee + "ml of water.");
+
+    }
 }
